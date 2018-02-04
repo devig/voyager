616030dcb3e8e96cf2df0368ed189da57a261c83
Julian <juliancamilleri1989@gmail.com>
2018-02-04 10:56:34 +0100

    Added to default bread

--- resources/views/bread/edit-add.blade.php
+++ resources/views/bread/edit-add.blade.php
@@ -125,41 +125,12 @@
 @stop
 
 @section('javascript')
+    @yield('pivot-javascript')
     <script>
         var params = {}
         var $image
 
         $('document').ready(function () {
-            $('.select-relationship-with-pivot').val('');
-            $('.select-relationship-with-pivot').on('change', function(e) {
-                var relationshipName = $(this).attr('data-relationship-field');
-                var pivotColumn = $(this).attr('data-pivot-column');
-
-                var value = $(this).val();
-                var name = $(this).find(':selected').html();
-
-                $(this).val('');
-                $(this).find('option[value='+ value +']').toggle();
-
-                var html = '<div class="selected-relationship" data-relationship-value="' + value + '">';
-                html += '<span>' + name + '</span>';
-                html += '<input type="text" value="' + value + '" name="' + relationshipName + '_ids[]" hidden>';
-                html += '<input class="form-control with-pivot-relationship-value" name="' + relationshipName + '_' + pivotColumn + '_values[]" type="text" placeholder="Column Value">';
-                html += '<span class="remove-relationship">Remove</span>'
-                html += '</div>';
-
-                $('.selected-relationship-with-pivot-container').append(html);
-            });
-
-            $('body').on('click', '.remove-relationship', function() {
-                var optionValue = $(this).closest('.selected-relationship').attr('data-relationship-value');
-                $('.select-relationship-with-pivot').find('option[value='+ optionValue +']').toggle();
-
-                if (confirm('Are you sure you want to remove this relationship?')) {
-                    $(this).closest('.selected-relationship').remove();
-                }
-            });
-
             $('.toggleswitch').bootstrapToggle();
 
             //Init datepicker for date fields if data-datepicker attribute defined
